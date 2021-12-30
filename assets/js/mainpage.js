706e132b63d7fd3c1796ed665acb2e093b4bc47b
ganbari <steinway.nathan@gmail.com>
2021-12-29 18:48:00 -0600

    added model dropdown button and functionality, a few more comments for readability, and created an add button for later use

--- assets/js/mainpage.js
+++ assets/js/mainpage.js
@@ -1,26 +1,13 @@
+var addBtn = document.getElementById("add-button");
 
+function justchecking(addBtn) {
 
+    if (addBtn.click) {
+      console.log("fuck");
+    } else {
+      console.log("you");
+    }
 
+  }
 
-/* TESTING PART TYPE BUTTON
-
-var testButton = function (event) {
-    console.log("I am working!");
-}
-
-var testButtonTwo = function (event) {
-    console.log("No, I'm working!");
-}
-
-var testButtonThree = function (event) {
-    console.log("No, I'M working!!!");
-}
-
-var testButtonFour = function (event) {
-    console.log("You guys...");
-}
-document.getElementById("part-type-btn").addEventListener('click', testButton);
-document.getElementById("series-btn").addEventListener('click', testButtonTwo);
-document.getElementById("model-btn").addEventListener('click', testButtonThree);
-document.getElementById("compare-btn").addEventListener('click', testButtonFour);
-*/
+addBtn.addEventListener("click", justchecking);
